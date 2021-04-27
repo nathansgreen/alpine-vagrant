a42adab90fba26ac573fb8443509cdacf37b26c5
Nathan Green <ngreen@inco5.com>
2021-04-27 12:19:27 -0500

    In update-iso-checksum.sh use gnu-sed if running on MacOS

--- update-iso-checksum.sh
+++ update-iso-checksum.sh
@@ -3,5 +3,6 @@
 set -eux
 iso_url=$(jq -r '.variables.iso_url' alpine.json)
 iso_checksum=$(curl -o- --silent --show-error $iso_url.sha256 | awk '{print $1}')
+case $(uname -s) in Darwin) alias sed=gsed ;; esac
 sed -i -E "s,(\"iso_checksum\": \")([a-f0-9]+|)(\"),\\1$iso_checksum\\3,g" alpine.json
 echo 'iso_checksum updated successfully'
